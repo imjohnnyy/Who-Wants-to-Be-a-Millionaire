7a1d31ac22b1883a887d776da6a5f28867997f3b
Johnny <ldong@192.168.1.69>
2021-06-02 11:24:10 +1200

    Corrected the output when successfully connected to the database

---
+++ src/pdc_assignment2/UserAnswersDBConnectionTester.java
@@ -0,0 +1,70 @@
+package pdc_assignment2;
+
+/**
+ *
+ * @author Johnny
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public final class UserAnswersDBConnectionTester {
+
+    private static final String USER_NAME = "abc"; 
+    private static final String PASSWORD = "abc"; 
+    private static final String DATABASE_URL = "jdbc:derby:UserAnswersDB_Ebd;create=true";  
+
+    Connection connection;
+    
+
+    public UserAnswersDBConnectionTester() {
+        establishConnection();
+    }
+
+     public Connection getConnection() {
+        return this.connection;
+    }
+
+    //Establishes a connection to database
+    public void establishConnection() {
+
+            try 
+            {
+                connection = DriverManager.getConnection(DATABASE_URL, USER_NAME, PASSWORD);
+                System.out.println(DATABASE_URL + " ,  Successfully Connected to the AudienceAnswers Database...");
+                
+            } 
+            catch (SQLException ex) 
+            {
+                System.out.println(ex.getMessage());
+            }
+        
+    }
+
+    public void closeConnections() 
+    {
+        if(this.connection != null)
+        {
+            try 
+            {
+                connection.close();
+            } 
+            catch (SQLException ex) 
+            {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        
+        UserAnswersDBConnectionTester dbTester = new UserAnswersDBConnectionTester();
+        System.out.println(dbTester.getConnection());
+        
+    }
+    
+   
+     
+}
